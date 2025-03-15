72f1314d9e43b5eaab1156d9af951eb40b3885d5
Amaira Aggarwal <amairaaggarwal9104@gmail.com>
2025-03-15 18:11:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+STOCK MARKET ANALYSIS
+
+1.  Data taken from yfinance
+2.  A prediction pipeline was also created
---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            area=float(request.form.get('area')),
+            bhk=request.form.get('bhk'),
+            type=request.form.get('type'),
+            status=request.form.get('status'),
+            parking=request.form.get('parking'),
+            furnishing=request.form.get('furnishing'),
+            transaction=request.form.get('transaction')
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1",port=5500)        
